d4bbb9974eb2ba89d289c6d97ae432691c05d3d9
Gabriel Castro <gabriel.castro@omio.com>
2026-04-25 00:18:11 +0200

    feat(web): overview/follow/manual view modes

--- web/src/components/Map/Map.tsx
+++ web/src/components/Map/Map.tsx
@@ -1,18 +1,24 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import { AircraftMarker } from './AircraftMarker.js';
 import { BreadcrumbTrail } from './BreadcrumbTrail.js';
+import { MapController } from './MapController.js';
 import { PlannedRoute } from './PlannedRoute.js';
+import { ViewModeControl } from './ViewModeControl.js';
 
 export function Map() {
   return (
-    <MapContainer center={[40, 0]} zoom={4} style={{ height: '100%', width: '100%' }} worldCopyJump>
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <PlannedRoute />
-      <BreadcrumbTrail />
-      <AircraftMarker />
-    </MapContainer>
+    <div style={{ position: 'relative', height: '100%', width: '100%' }}>
+      <MapContainer center={[40, 0]} zoom={4} style={{ height: '100%', width: '100%' }} worldCopyJump>
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <PlannedRoute />
+        <BreadcrumbTrail />
+        <AircraftMarker />
+        <MapController />
+      </MapContainer>
+      <ViewModeControl />
+    </div>
   );
 }
